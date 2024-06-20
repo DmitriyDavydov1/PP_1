6bf68ff90b97a673d579a199e5d67de470fdedee
Dmitriy Davydov <koldoond@gmail.com>
2024-06-20 16:22:10 +0400

    init get_data

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,11 @@
-import masks
+from masks import get_mask_account
+from masks import get_mask_card_number
 
 
-def mask_account_card(account_card: str, ) -> str:
+def mask_account_card(account_card: str) -> str:
+    "Функция, которая преобразует номер карты и счета"
+
+
     account_card_new = account_card.split()
     name_mask = []
     mask = ""
@@ -11,15 +15,33 @@ def mask_account_card(account_card: str, ) -> str:
             name_mask.append(i)
         else:
             mask = i
-    print(name_mask)
-    print(mask)
-    if 'Счет' or 'Счёт' or "счет" or 'счёт' in name_mask:
-        mask1 = masks.get_mask_account(mask)
+    if 'Счет' in name_mask:
+        mask1 = get_mask_account(mask)
+    elif 'Счёт' in name_mask:
+        mask1 = get_mask_account(mask)
+    elif 'счет' in name_mask:
+        mask1 = get_mask_account(mask)
+    elif 'счёт' in name_mask:
+        mask1 = get_mask_account(mask)
     else:
-        mask1 = masks.get_mask_card_number(mask)
+        mask1 = get_mask_card_number(mask)
     name_mask_new = " ".join(name_mask)
     mask_account = name_mask_new + " " + mask1
     return mask_account
 
 
-print(mask_account_card("Visa 1234567891234567"))
+print(mask_account_card("Счет 12345678912345678912"))
+print(mask_account_card("Visa Classic 1234567891234567"))
+print(mask_account_card("Mastercard 9874567891231212"))
+
+
+
+from datetime import datetime
+def get_data(data: str): -> str:
+    "Функция которая преобразует дату"
+
+
+    d = datetime.strptime(data, format("%Y-%m-%dT%H:%M:%S:%F"))
+    return d.strftime("%d.%M.%Y")
+
+get_data(2018-07-11T02:26:18.671407)
