bd7791d2fbcb6ce7fea612c267903204f3810ab3
Dmitriy Davydov <koldoond@gmail.com>
2024-06-21 12:18:11 +0400

    the finished version

--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,10 @@
 from masks import get_mask_account
 from masks import get_mask_card_number
+from datetime import datetime
 
 
 def mask_account_card(account_card: str) -> str:
-    "Функция, которая преобразует номер карты и счета"
-
+    """Функция, которая преобразует номер карты и счета"""
 
     account_card_new = account_card.split()
     name_mask = []
@@ -15,33 +15,24 @@ def mask_account_card(account_card: str) -> str:
             name_mask.append(i)
         else:
             mask = i
-    if 'Счет' in name_mask:
-        mask1 = get_mask_account(mask)
-    elif 'Счёт' in name_mask:
-        mask1 = get_mask_account(mask)
-    elif 'счет' in name_mask:
-        mask1 = get_mask_account(mask)
-    elif 'счёт' in name_mask:
+    if 'Счет' in name_mask or 'Счёт' in name_mask or 'счет' in name_mask or 'счёт' in name_mask:
         mask1 = get_mask_account(mask)
     else:
         mask1 = get_mask_card_number(mask)
-    name_mask_new = " ".join(name_mask)
-    mask_account = name_mask_new + " " + mask1
+    mask_account = " ".join(name_mask) + " " + mask1
     return mask_account
 
 
-print(mask_account_card("Счет 12345678912345678912"))
-print(mask_account_card("Visa Classic 1234567891234567"))
-print(mask_account_card("Mastercard 9874567891231212"))
+mask_account_card("Счет 12345678912345678912")
+mask_account_card("Visa Classic 1234567891234567")
+mask_account_card("Mastercard 9874567891231212")
 
 
+def get_data(data_string: str) -> str:
+    """Функция которая преобразует дату"""
 
-from datetime import datetime
-def get_data(data: str): -> str:
-    "Функция которая преобразует дату"
+    d = datetime.strptime(data_string, format("%Y-%m-%dT%H:%M:%S.%f"))
+    return d.strftime(format("%d.%m.%Y"))
 
 
-    d = datetime.strptime(data, format("%Y-%m-%dT%H:%M:%S:%F"))
-    return d.strftime("%d.%M.%Y")
-
-get_data(2018-07-11T02:26:18.671407)
+get_data("2018-07-11T02:26:18.671407")
