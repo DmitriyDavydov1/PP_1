73b53a640ac071ebc7c59fc2ab41efda3b5a0a66
Dmitriy Davydov <koldoond@gmail.com>
2024-06-21 13:17:39 +0400

    import correction

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,5 @@
-from masks import get_mask_account
-from masks import get_mask_card_number
+from src.masks import get_mask_account
+from src.masks import get_mask_card_number
 from datetime import datetime
 
 
