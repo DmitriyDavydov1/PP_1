9dd64918515f72d61385452792bd8eb47d792a15
Dmitriy Davydov <koldoond@gmail.com>
2024-07-29 09:45:19 +0400

    pr1

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -50,7 +50,7 @@ def test_sort_by_date_param(list_init, state_init, list_filter):
                     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
                     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
                 ],
-                'True',
+                True,
                 [
                     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
                     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
@@ -66,12 +66,13 @@ def test_sort_by_date_param(list_init, state_init, list_filter):
                            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
                            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
                        ],
-                       'False',
+                       False,
                        [
-                           {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                           {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                           {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
                            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                           {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
+                           {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                           {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}
+
                        ]
                 )
         )
