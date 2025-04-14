fda20f01290bdc5b373a8c66821293fb549b04e0
Davydov_Dmitry <koldoond@gmail.com>
2025-04-14 14:44:13 +0400

    merch

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import os
 
 logger = logging.getLogger("utils")
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler("logs/utils.log")
+file_handler = logging.FileHandler("../logs/utils.log")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
@@ -13,10 +13,6 @@ logger.addHandler(file_handler)
 def list_from_json(path_to_file):
     """Функция, которая принимает на вход путь до JSON-файла и возвращает список словарей с данными о транзакциях.
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список."""
-
-    if not os.path.exists(path_to_file):
-        logger.warning(f"Указан несуществующий путь: {path_to_file}")
-        return []
 
     try:
         with open(path_to_file, 'r', encoding='utf-8') as file:
