f53faa8fdd65dcc37c6794230f0bc45a64a9b9b6
Davydov_Dmitry <koldoond@gmail.com>
2025-04-18 06:38:05 +0400

    13-2 date_z

--- src/widget.py
+++ src/widget.py
@@ -3,28 +3,16 @@ from src.masks import get_mask_card_number
 from datetime import datetime
 
 
-def mask_account_card(account_card: str) -> str:
-    """Функция, которая преобразует номер карты и счета"""
-
-    account_card_new = account_card.split()
-    name_mask = []
-    mask = ""
-    for i in account_card_new:
-        a = i.isalpha()
-        if a:
-            name_mask.append(i)
-        else:
-            mask = i
-    if "Счет" in name_mask or "Счёт" in name_mask or "счет" in name_mask or "счёт" in name_mask:
-        mask1 = get_mask_account(mask)
+def mask_account_card(type_and_number: str) -> str:
+    """Обрабатывает информацию о картах и о счетах"""
+    if type_and_number[:4].lower() in ["счет", "счёт"]:
+        return f"{type_and_number[:4].title()} **{type_and_number[-4:]}"
     else:
-        mask1 = get_mask_card_number(mask)
-    mask_account = " ".join(name_mask) + " " + mask1
-    return mask_account
+        return f"{type_and_number[:-12]} {type_and_number[-12:-10]}** **** {type_and_number[-4:]}"
 
 
 def get_date(datetime_str: str) -> str:
     """Меняет формат даты с datetime на привычный ДД.ММ.ГГГГ"""
-    datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S.%f")
+    datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%SZ")
     new_format_date = datetime_obj.strftime("%d.%m.%Y")
     return new_format_date
